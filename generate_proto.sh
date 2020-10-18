ed1492f9e70b6b5ba56906abf9458b4b2f0ab993
Sakthi N <sakthi.n@zohocorp.com>
2020-10-18 21:24:26 +0530

    updated proto generation script to use module instead of deprecated relative path options

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,4 +1,5 @@
-protoc -I proto/ proto/account/v1/user.proto --go_out=paths=source_relative:.
-protoc -I proto/ proto/buyer/v1/buyer.proto --go_out=paths=source_relative:.
-protoc -I proto/ proto/seller/v1/seller.proto --go_out=paths=source_relative:.
-protoc -I proto/ proto/transporter/v1/transporter.proto --go_out=paths=source_relative:.
+protoc -I proto/ proto/account/v1/user.proto --go_out=module=github.com/ihtkas/farm:.
+protoc -I proto/ proto/buyer/v1/buyer.proto --go_out=module=github.com/ihtkas/farm:.
+protoc -I proto/ proto/seller/v1/seller.proto --go_out=module=github.com/ihtkas/farm:.
+protoc -I proto/ proto/transporter/v1/transporter.proto --go_out=module=github.com/ihtkas/farm:.
+protoc -I. --go_out=module=github.com/ihtkas/farm:. --go-grpc_out=module=github.com/ihtkas/farm:. proto/account/v1/auth.proto
